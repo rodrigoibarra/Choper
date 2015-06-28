a33c473828afbd98532ec33d0a2964b1a15baa2a
rodrigoibarra <hola@rodrigo-ibarra.com>
2015-06-28 15:44:28 -0500

    se agrega menu responsivo
    Se agrega una funcionalidad responsiva para el menu de navegación principal.

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,6 +1,6 @@
 module.exports = {
     build: {
-      src: ['_dev/libs/*.js','_dev/js/global.js'],
+      src: ['_dev/libs/*.js', '_dev/js/global.js'],
       dest: 'build/js/global.min.js'
   }
 }
